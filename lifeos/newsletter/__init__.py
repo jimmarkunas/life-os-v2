600973bde4bee21340082cb7380f84a792756e07
Jim Markunas <248513254+jimmarkunas@users.noreply.github.com>
2026-09-19 13:12:43 -0500

    Retire Newsletter Jobs seam

--- lifeos/newsletter/__init__.py
+++ lifeos/newsletter/__init__.py
@@ -1,6 +1,5 @@
 """Routed Newsletter source processing."""
-from .jobs_seam import JobsCandidateAdapter, adapt_for_jobs
 from .models import MessageParseResult, ParseState, RoutedNewsletterMessage, SourceVacancyObservation
 from .parsers import detect_source, parse_message
 from .processor import NewsletterExecutionState, NewsletterProcessResult, NewsletterProcessor, NewsletterSourcePort
-__all__ = ["JobsCandidateAdapter","adapt_for_jobs","MessageParseResult","ParseState","RoutedNewsletterMessage","SourceVacancyObservation","detect_source","parse_message","NewsletterExecutionState","NewsletterProcessResult","NewsletterProcessor","NewsletterSourcePort"]
+__all__ = ["MessageParseResult","ParseState","RoutedNewsletterMessage","SourceVacancyObservation","detect_source","parse_message","NewsletterExecutionState","NewsletterProcessResult","NewsletterProcessor","NewsletterSourcePort"]
